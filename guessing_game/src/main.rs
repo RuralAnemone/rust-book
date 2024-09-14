ce01a16b2e4f5c5b44d1ad0d452c22dceb497670
Isaiah W <isaiah.wallerstedt@proton.me>
2024-09-14 12:54:55 -0500

    default program

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("fun number guessin game");
+
+    println!("guess a number!:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("uh oh couldn't read the line :0");
+
+    println!("your guess was: {}", guess);
 }
