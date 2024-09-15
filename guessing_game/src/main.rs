8e37527b03067e9d5e8a6a60a879efd5fbed65f3
Isaiah W <isaiah.wallerstedt@proton.me>
2024-09-15 13:23:21 -0500

    WAIT YOU CAN JUST CHANGE TYPES

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("fun number guessin game");
@@ -16,5 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("uh oh couldn't read the line :0");
 
+    let guess: u32 = guess.trim().parse().expect("that's  not a number numbnuts. try again");
+
     println!("your guess was: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("too small (that's what she said)"),
+        Ordering::Greater => println!("too big! (that ain't what she said)"),
+        Ordering::Equal => println!("you win I guess"),
+    }
 }
