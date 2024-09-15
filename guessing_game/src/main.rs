683e94d03b4a714301dbbbccb7a7d47c4631f9d5
Isaiah W <isaiah.wallerstedt@proton.me>
2024-09-15 13:08:45 -0500

    rnadom functionality

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("fun number guessin game");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("the secret nubmer is {secret_number}");
 
     println!("guess a number!:");
 
